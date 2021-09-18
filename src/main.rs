efea77e8a7f9a20afd9295644b9a97b0e529afba
Seth Stadick <sstadick@gmail.com>
2021-09-18 07:46:07 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use structopt::{clap::AppSettings::ColoredHelp, StructOpt};
+
+pub mod built_info {
+    use structopt::lazy_static::lazy_static;
+
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+
+    /// Get a software version string including
+    ///   - Git commit hash
+    ///   - Git dirty info (whether the repo had uncommitted changes)
+    ///   - Cargo package version if no git info found
+    fn get_software_version() -> String {
+        let prefix = if let Some(s) = GIT_COMMIT_HASH {
+            format!("{}-{}", PKG_VERSION, s[0..8].to_owned())
+        } else {
+            // This shouldn't happen
+            PKG_VERSION.to_string()
+        };
+        let suffix = match GIT_DIRTY {
+            Some(true) => "-dirty",
+            _ => "",
+        };
+        format!("{}{}", prefix, suffix)
+    }
+
+    lazy_static! {
+        /// Version of the software with git hash
+        pub static ref VERSION: String = get_software_version();
+    }
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "bundle-licenses", author, global_setting(ColoredHelp), version = built_info::VERSION.as_str())]
+pub struct Opts {}
+
+fn main() {
+    println!("Hello, world!");
+}
